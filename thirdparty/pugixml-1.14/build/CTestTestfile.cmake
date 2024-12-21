7c113a457b7f7c5e74406bc7a3b75d4e71838755
ahiliitb <126464160+ahiliitb@users.noreply.github.com>
2024-12-22 02:28:18 +0530

    Delete thirdparty/pugixml-1.14 directory

--- thirdparty/pugixml-1.14/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahilkhanjnv/MYWORK/safesquid/cpp-superset-reporting/thirdparty/pugixml-1.14
-# Build directory: /home/ahilkhanjnv/MYWORK/safesquid/cpp-superset-reporting/thirdparty/pugixml-1.14/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
